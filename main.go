ec337ee975e5f573a0dded516614129660229dbf
Pranshul Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2025-07-03 12:19:52 +0530

    added

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+// func display() int {
+// 	var a int
+// 	fmt.Print("Enter value for a : ")
+// 	fmt.Scan(&a)
+// 	fmt.Printf("The value of a is : %v\n", a)
+// 	return a
+// }
+
+func main() {
+	// // VARIABLE DECLARATION
+
+	// // Explicit
+	// var a int = 10
+	// var b float64 = 1.2
+	// var c string = "salutations"
+	// var d bool = true
+	// var a1 int32
+
+	// // Type inference
+	// var a2 = 12
+
+	// // Short-hand
+	// d1 := false
+
+	// // Constant
+	// const f int = 25
+
+	// fmt.Printf("Explicit declaration of variables : %v, %v, %v, %v, %v \nType Inference declaration of variables : %v
+	// \nShort-hand declaration of variables : %v \nConstants declaration : %v", a, b, c, d, a1, a2, d1, f)
+
+	// // FUNCTION CALLING
+
+	// val := display()
+	// fmt.Printf("Returned value : %v", val)
+
+	// // ARRAY AND SLICE DECLARATION
+
+	// var arr = [3]int{1, 2, 3}
+	// var arr1 = [0]int{}
+
+	// var nums = []int{4, 5}
+	// var nums1 = []int{}
+
+	// // Appending elements in the slices
+	// nums = append(nums, 8)
+	// nums1 = append(nums1, 5)
+
+	// // Removing elements from slices
+	// i := 1
+	// nums = append(nums[:i], nums[i+1:]...)
+
+	// // Printing the elements
+	// fmt.Println("arr : ", arr)
+	// fmt.Println("arr1 : ", arr1)
+	// fmt.Println("nums : ", nums)
+	// fmt.Println("nums1 : ", nums1)
+
+	// // The size and capacity of arrays and slices
+	// fmt.Println("Length & Capacity:")
+	// fmt.Printf("arr -> len: %d, cap: %d\n", len(arr), cap(arr))
+	// fmt.Printf("arr1 -> len: %d, cap: %d\n", len(arr1), cap(arr1))
+	// fmt.Printf("nums -> len: %d, cap: %d\n", len(nums), cap(nums))
+	// fmt.Printf("nums1 -> len: %d, cap: %d\n", len(nums1), cap(nums1))
+
+}
